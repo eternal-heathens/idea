2453e916b6405d9477b6bf063a88cafb66c74655
GitHub Action <action@github.com>
2023-04-17 08:47:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/eternalheathens/idea/MyBundle.kt
+++ src/main/kotlin/com/github/eternalheathens/idea/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.eternalheathens.idea
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/eternalheathens/idea/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/eternalheathens/idea/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.eternalheathens.idea.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
